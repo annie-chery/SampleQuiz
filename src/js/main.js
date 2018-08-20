0584361a9e32b69568d891fa6d060fdc29b538e4
Annie Cherian <cherian@soapcreative.com>
2018-08-20 17:05:29 +1000

    Added html for radio buttons and the submit button

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,97 @@
 
-import questions from './questions.json'
+import questions from './questions.json';
 
+var i= 0;
+var score = 0;
 var page = questions.questions;
+var timeClicked = 0;
 
-for(var i=0; i < page.length; i++){
-	document.getElementById('question').innerHTML = page[1].text
+
+
+generateQuestion(0);
+//console.log(page.length);
+
+function _(id){
+ return document.getElementById(id);
 }
 
-console.log("Checked n verified");
+
+
+function generateQuestion(i){
+	_('right-answer').innerHTML = ""
+	_("opt1").checked = false;
+	_("opt2").checked = false;
+	_("opt3").checked = false;
+	_('question').innerHTML = page[i].heading;
+	_('question-desc').innerHTML = page[i].text_description;
+	_('optt1').innerHTML = page[i].choices[0]
+	_('optt2').innerHTML = page[i].choices[1]
+	_('optt3').innerHTML = page[i].choices[2]
+	_('optt1').value = page[i].choices[0]
+	_('optt2').value = page[i].choices[1]
+	_('optt3').value = page[i].choices[2]
+
+}
+function nextQuestion(i){
+	_("btn-cont").addEventListener('click',function(e){
+
+		if(page.length > i){
+			generateQuestion(i);
+		} else{
+			_('block-quiz').innerHTML = "Your score is "+ score;
+		}
+	});
+
+
+
+}	
+	
+
+function checkAnswer(){
+	//e.preventDefault();
+	
+	if(_("opt1").checked && (_('optt1').value === page[i].answer)){
+		//_("opt1").value  = page[i].choices[0];
+		score++;
+
+	} else{
+		_('right-answer').innerHTML = page[i].answer;
+	}
+	if(_("opt2").checked &&(_('optt2').value === page[i].answer )){
+		//_("opt2").value  = page[i].choices[1];
+		score++;
+		
+	} else{
+		_('right-answer').innerHTML = page[i].answer;
+	}
+	
+	if(_("opt3").checked &&  _('optt3').value === page[i].answer){
+		//_("opt3").value  = page[i].choices[2];
+		score++;
+		
+	} else{
+		_('right-answer').innerHTML = page[i].answer;
+	}
+	i++;
+	_('btn-cont').innerHTML  = "Continue";
+
+	nextQuestion(i);
+	
+}
+
+_("btn-cont").addEventListener('click',function(e){
+	var timesClicked =1;
+
+	e.preventDefault();
+	if( timesClicked % 2 != 0){
+		timesClicked++;
+		_('btn-cont').innerHTML  = "Submit Answer";
+		checkAnswer();
+	}
+	
+	
+	
+})
 
 // This section is added to avoid the hot module error and warning saying full reload is needed
 if (module.hot) {
